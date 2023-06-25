d6c414a7fd65bdc0c6d25702d93ee537bc217788
Map4che <ljuan648l@gmail.com>
2023-06-25 15:48:12 -0500

    Arreglo definitivo Login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,15 +1,31 @@
 import { Link } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
   return (
     <div class="container-login">
       <div class="login">
-        <img src="" alt="Logo" class="logo-login" />
-        <label htmlFor="email">Correo Electronico</label>
-        <input type="email" id="email" name="email" required />
-        <label htmlFor="contrasenia">Contraseña</label>
-        <input type="password" id="contrasenia" required />
-        <input type="submit" id="input-boton" value="Ingresa" />
+        <div class="login__image-y-titulo">
+          <img src="" alt="Logo" class="logo-login" />
+          <h1 class="login__titulo">Inicie su sesión</h1>
+        </div>
+        <label htmlFor="email">Usuario:</label>
+        <input
+          type="email"
+          class="form-input"
+          id="email"
+          name="email"
+          placeholder="Correo electronico"
+          required
+        />
+        <label htmlFor="contrasenia">Contraseña:</label>
+        <input type="password" class="form-input" id="contrasenia" required />
+        <input
+          type="submit"
+          class="input-boton"
+          value="Ingresa"
+          name="password"
+        />
       </div>
       <p>
         Olvidaste tu contraseña? Haz click{" "}
